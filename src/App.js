9b9ee0686385181d643294c36e2b2d5ed1a853c1
adrianomsjf <adrianomsjf@gmail.com>
2023-04-14 08:32:33 -0300

    Primeiro teste deploy front

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
 
 function App() {
+
+   async function getback() {
+      const ret = await fetch('https://teste-back-gui.onrender.com')
+      const data = await ret.text()
+      setRetApi(data)
+      console.log(data)
+   }
+
+   useEffect(()=>{
+      getback()
+   },[])
+
+   const [retapi,setRetApi] = useState()
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>Teste Front Gui</div>
+      {retapi && <div>{retapi}</div>}
     </div>
   );
 }
